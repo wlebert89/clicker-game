8197f92e21fcd109dd05ebb1cf45e5cdf3abbea4
lqdsqd <wesley.lebert@gmail.com>
2019-04-30 22:23:37 -0500

    Updates

---
+++ precache-manifest.8c38dee93d78f36c3bb3c8d11d579b5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2cb114effc412e8219f2fb7aa3dd597",
+    "url": "/clicker-game/index.html"
+  },
+  {
+    "revision": "84fa95b273653eec187c",
+    "url": "/clicker-game/static/css/main.1acecac6.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicker-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "84fa95b273653eec187c",
+    "url": "/clicker-game/static/js/main.6e4971cb.chunk.js"
+  },
+  {
+    "revision": "cd401eb4e9e99ab75dd2",
+    "url": "/clicker-game/static/js/runtime~main.8efb6c9b.js"
+  }
+]);
